e77bd0a87c0f422701463d89881eadaec6a9d199
OBE96 <132613977+OBE96@users.noreply.github.com>
2024-07-30 21:05:26 +0100

    initial_commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const wedgeMovies = films.filter((film) =>
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+    console.log(wedgeMovies.length);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+#### LEARNING Web Scraping
